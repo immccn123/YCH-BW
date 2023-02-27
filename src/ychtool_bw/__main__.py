b09a364cbb92b92f2fe564e02611b489fea25625
Youxi Luo <immccn123@outlook.com>
2023-02-27 12:55:03 +0000

    init project

---
+++ src/ychtool_bw/__main__.py
@@ -0,0 +1,4 @@
+from ychtool_bw.app import main
+
+if __name__ == '__main__':
+    main().main_loop()
---
+++ src/ychtool_bw/app.py
@@ -0,0 +1,27 @@
+"""
+A tool for Onion School to do the homework automatically.
+"""
+import toga
+from toga.style import Pack
+from toga.style.pack import COLUMN, ROW
+
+
+class YCHBW(toga.App):
+
+    def startup(self):
+        """
+        Construct and show the Toga application.
+
+        Usually, you would add your application to a main content box.
+        We then create a main window (with a name matching the app), and
+        show the main window.
+        """
+        main_box = toga.Box()
+
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.main_window.content = main_box
+        self.main_window.show()
+
+
+def main():
+    return YCHBW()
